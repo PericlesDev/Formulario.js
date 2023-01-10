6ced2a143b9cc45355afbc652c185d06680af4e1
matheus santos <PericlesDosSantosRamos@hotmail.com>
2023-01-10 17:41:59 -0300

    Validando formulário inicialmente com o regex

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const isValidEmail = (email) => {
+    const regex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(String(email).toLowerCase())
+  }
+  
+  const isValidCPF = (cpf) => {
+    const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+    return regex.test(String(cpf).toLowerCase())
+  }
